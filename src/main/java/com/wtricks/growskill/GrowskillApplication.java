7cb1d4d081f689b02d61222c21aa5c5fc8e329ed
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-09-09 09:50:23 +0530

    Add files via upload

---
+++ src/main/java/com/wtricks/growskill/GrowskillApplication.java
@@ -0,0 +1,13 @@
+package com.wtricks.growskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowskillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowskillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wtricks/growskill/GrowskillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtricks.growskill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowskillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
